f8724e08aef4fcb507a4f2f04493270ebdb1b193
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-04-17 14:54:02 -0400

    yeah

---
+++ grade.js
@@ -0,0 +1,9 @@
+let myAverage = 59
+
+if (myAverage <= 59) {
+    console.log("You're failing. (F)")
+}   else {
+    console.log("Your passing, but at what cost?")
+    else if (myAverage >= 59)
+    console.log("Your passing, yay!")
+}
---
+++ hello.js
@@ -0,0 +1,18 @@
+console.log("Hello World!")
+
+let myName = "Kasim"
+let myAge = 17
+
+console.log("My name is " + myName + ". My age is " + myAge + ".")
+
+if (myAge == 17) {
+    console.log("I'm a year away from turning 18!")
+} else {
+    console.log("I was born in the year of 2006.")
+}
+
+let myList = [Siege, TWOK, Fortnite]
+
+for (let item of myList) {
+    console.log("item")
+}
