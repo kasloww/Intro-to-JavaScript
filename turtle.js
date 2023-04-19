c3eb9d20b6dcf74316ac4814ebe1fde4f9b316bb
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-04-19 14:52:56 -0400

    house

---
+++ turtle.js
@@ -0,0 +1,23 @@
+let aquarium = document.getElementById("aquarium")
+
+let turtle = new RealTurtle.default(aquarium, {autoStart: true})
+
+turtle.setPosition(200, 50)
+
+turtle.beginPath()
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(30)
+turtle.forward(30)
+turtle.right(105)
+turtle.forward(36)
+
+turtle.setPosition(210, 55)
+
