50aea9d0ead1e628b88d3b219b092eb62bba65c7
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-04-28 14:52:12 -0400

    stop

---
+++ stopwatch.js
@@ -0,0 +1,12 @@
+function start () {
+    alert("You clicked the start button")
+    
+}
+
+function stop () {
+    alert("You clicked the stop button")
+}
+
+function reset () {
+    alert("You clicked the reset button")
+}
