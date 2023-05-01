5bb43a11b11b7defb7ea46bbb14ccbf269cf9f90
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-05-01 13:10:05 -0400

    fixing stopwatch

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,13 @@
+let seconds = 0
+let milliseconds = 0
+let minutes = 0
+
 function start () {
     alert("You clicked the start button")
-    
+
+    setInterval(function () {
+        console.log("Hi")
+    }, 1000)
 }
 
 function stop () {
