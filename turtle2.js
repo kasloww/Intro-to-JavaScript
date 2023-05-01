18cdf0bea428b7a1206ca529428d0d36e79a9dd3
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-05-01 13:10:29 -0400

    stopwatch

--- turtle2.js
+++ turtle2.js
@@ -2,13 +2,13 @@ let aquarium = document.getElementById("aquarium")
 
 let turtle = new RealTurtle.default(aquarium, {autoStart: true})
 
-function polygon(sides, size) {
+function polygon(sides, size = 20) {
     for (let i = 0; i < sides; i++) {
         turtle.forward(size)
-        turtle.right(360 / sides)
+        turtle.right(360/sides)
     }
 }
 
-polygon(10, 10)
-
-polygon(10)
+for (let i = 0; i < 5; i ++) {
+    polygon((Math,random() = 10) + 3)
+}
