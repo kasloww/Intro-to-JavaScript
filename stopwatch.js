3a10cbc8435eaa7dc2361c4b74e796848c927dbc
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-05-01 14:53:22 -0400

    fixed it all

--- stopwatch.js
+++ stopwatch.js
@@ -2,18 +2,36 @@ let seconds = 0
 let milliseconds = 0
 let minutes = 0
 
-function start () {
-    alert("You clicked the start button")
+let digits = document.getElementById('digits')
 
-    setInterval(function () {
-        console.log("Hi")
-    }, 1000)
+let interval;
+
+function start () {
+reset()
+
+    interval = setInterval(function () {
+        milliseconds = milliseconds + 10
+        if (milliseconds >= 1000) {
+            seconds = seconds + 1
+            milliseconds = 0
+        }
+        if (seconds >= 60) {
+            minutes += 1
+            seconds = 0
+        }
+        digits.innerHTML = minutes + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds
+    }, 10)
 }
 
 function stop () {
-    alert("You clicked the stop button")
+   clearInterval(interval)
 }
 
 function reset () {
-    alert("You clicked the reset button")
+    stop()
+    seconds = 0
+    milliseconds = 0
+    minutes = 0
+    digits.innerHTML = minutes + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds
+
 }
